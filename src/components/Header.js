3c7fccbd92b2e702e6d8738a28a47808e99fd2a8
Charlie Astrada <carlosastrada@gmail.com>
2020-03-05 01:50:26 -0300

    Add Header styling using material ui and custom theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { GraphicEq } from "@material-ui/icons";
+
+const useStyles = makeStyles(theme => ({
+  title: { marginLeft: theme.spacing(2) }
+}));
 
 function Header() {
-  return <div>Header</div>;
+  const classes = useStyles();
+
+  return (
+    <AppBar color="secondary" position="fixed">
+      <Toolbar>
+        <GraphicEq />
+        <Typography className={classes.title} variant="h6" component="h1">
+          Shared Music Hub
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
