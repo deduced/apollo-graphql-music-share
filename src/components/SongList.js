2896bcdbeb2d9036daf482e52daca43aa1f2a19a
Charlie Astrada <carlosastrada@gmail.com>
2020-03-08 13:54:29 -0300

    Create and style SongList component
    * Add Song component to this component -- should be moved to its own component.
    * used dummy data for populating the SongList for now. It will eventually be hooked into a database.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,97 @@
 import React from "react";
+import {
+  CircularProgress,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  IconButton,
+  makeStyles
+} from "@material-ui/core";
+import { PlayArrow, Save } from "@material-ui/icons";
 
 function SongList() {
-  return <div>SongList</div>;
+  let loading = false;
+
+  const song = {
+    title: "Ibiza Summer Mix",
+    artist: "Ibiza",
+    thumbnail: "http://i3.ytimg.com/vi/0IA1vCyffos/hqdefault.jpg"
+  };
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 50
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
+  return (
+    <div>
+      {Array.from({ length: 10 }, () => song).map((song, idx) => (
+        <Song key={idx} song={song} />
+      ))}
+    </div>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    margin: theme.spacing(3)
+  },
+  songInfoContainer: {
+    display: "flex",
+    alignItems: "center"
+  },
+  songInfo: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  thumbnail: {
+    objectFit: "cover",
+    width: 140,
+    height: 140
+  }
+}));
+
+function Song({ song }) {
+  const classes = useStyles();
+  const { artist, thumbnail, title } = song;
+
+  return (
+    <Card className={classes.container}>
+      <div className={classes.songInfoContainer}>
+        <CardMedia image={thumbnail} className={classes.thumbnail} />
+        <div className={classes.songInfo}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body1" component="p" color="textSecondary">
+              {artist}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton size="small" color="primary">
+              <PlayArrow />
+            </IconButton>
+            <IconButton size="small" color="secondary">
+              <Save />
+            </IconButton>
+          </CardActions>
+        </div>
+      </div>
+    </Card>
+  );
 }
 
 export default SongList;
